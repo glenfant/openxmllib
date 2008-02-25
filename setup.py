b92897c7f9c69b8b727e41adcbac23baedfebafa
kev <kev@coolcavemen.com>
2008-02-25 15:48:15 +0000

    Massive changes: use setuptools now for egg-ification.

--- setup.py
+++ setup.py
@@ -1,24 +1,41 @@
-#! /usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-% python setup.py install
 $Id: setup.py 6864 2007-12-06 14:09:09Z glenfant $
 """
-from distutils.core import setup
 
-setup(name='openxmllib',
-      version='1.0.0',
-      description='Open XML document library',
-      long_description='Open XML document library',
-      author='Gilles Lenfant',
-      author_email='gilles.lenfant@gmail.com',
-      url='http://code.google.com/p/openxmllib/',
-      license="GPLv2",
-      packages=['openxmllib'],
-      scripts=['scripts/openxmlinfo.py']
-      )
+from setuptools import setup, find_packages
+import os
 
-try:
-    import lxml
-except ImportError, e:
-    print "openxmllib will not work without lxml. Please install it (see README)"
+
+def read(*rnames):
+  return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+
+name             = 'openxmllib'
+version          = '1.0.1'
+long_description = "%s\n%s\n%s" % (read('README'), read('TODO'), read('HISTORY'))
+
+
+setup( name                 = name
+     , version              = version
+     , description          = 'Provides resources to handle OpenXML documents from Python.'
+     , long_description     = long_description
+     , author               = 'Gilles Lenfant'
+     , author_email         = 'gilles.lenfant@gmail.com'
+     , url                  = 'http://code.google.com/p/openxmllib/'
+     , license              = "GPLv2"
+     , keywords             = 'Python OpenXML lxml Office2007 ECMA376'
+     , classifiers          = [ "License :: OSI Approved :: GNU General Public License (GPL)"
+                              , "Topic :: Office/Business :: Office Suites"
+                              , "Topic :: Software Development :: Libraries :: Python Modules"
+                              , "Topic :: Utilities"
+                              , "Programming Language :: Python"
+                              ]
+     , packages             = find_packages(name, exclude='tests')
+     , package_dir          = {'': name}
+     , include_package_data = True
+     , install_requires     = ['lxml>=1.3.0,<2.0.0dev', ]
+     , zip_safe             = False
+     , scripts              = ['scripts/openxmlinfo.py', ]
+     )
+
