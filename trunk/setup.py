39156ccbe8ba3882c9e247ed693440df60395b7b
gilles.lenfant <gilles.lenfant@5448940a-3a44-0410-9b85-41ab8500f152>
2008-11-21 11:33:16 +0000

    Preparing release 1.0.3.

--- trunk/setup.py
+++ trunk/setup.py
@@ -12,26 +12,24 @@ name             = 'openxmllib'
 version          = '1.0.3'
 long_description = "%s\n%s\n%s" % (read('README'), read('TODO'), read('HISTORY'))
 
-
-setup( name                 = name
-     , version              = version
-     , description          = 'Provides resources to handle OpenXML documents from Python.'
-     , long_description     = long_description
-     , author               = 'Gilles Lenfant'
-     , author_email         = 'gilles.lenfant@gmail.com'
-     , url                  = 'http://code.google.com/p/openxmllib/'
-     , license              = "GPLv2"
-     , keywords             = 'Python OpenXML lxml Office2007 ECMA376'
-     , classifiers          = [ "License :: OSI Approved :: GNU General Public License (GPL)"
-                              , "Topic :: Office/Business :: Office Suites"
-                              , "Topic :: Software Development :: Libraries :: Python Modules"
-                              , "Topic :: Utilities"
-                              , "Programming Language :: Python"
-                              ]
-     , packages             = [name, ]
-     , include_package_data = True
-     , install_requires     = ['lxml>=1.3.0,<2.0.0dev', ]
-     , zip_safe             = False
-     , scripts              = ['scripts/openxmlinfo.py', ]
+setup(name=name,
+      version=version,
+      description='Provides resources to handle OpenXML documents from Python.',
+      long_description=long_description,
+      author='Gilles Lenfant',
+      author_email='gilles.lenfant@gmail.com',
+      url='http://code.google.com/p/openxmllib/',
+      license="GPLv2",
+      keywords='Python OpenXML lxml Office2007 ECMA376',
+      classifiers=["License :: OSI Approved :: GNU General Public License (GPL)",
+                   "Topic :: Office/Business :: Office Suites",
+                   "Topic :: Software Development :: Libraries :: Python Modules",
+                   "Topic :: Utilities",
+                   "Programming Language :: Python"
+                   ],
+      packages=[name],
+      include_package_data=True,
+      install_requires=['lxml>=1.3.0,<2.0.0dev'],
+      zip_safe=False,
+      scripts=['scripts/openxmlinfo.py']
      )
-
