a1bbd2993b6fb7d501f1912d1394e78ff5354ce5
gilles.lenfant <gilles.lenfant@5448940a-3a44-0410-9b85-41ab8500f152>
2009-10-31 16:11:43 +0000

    Fixed version and added credits to Kev and Hugo.

--- trunk/setup.py
+++ trunk/setup.py
@@ -5,16 +5,16 @@ import os
 import openxmllib
 
 def read(*rnames):
-  return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+  return open(os.path.join(os.path.dirname(__file__), *rnames)).read().strip()
 
 
 name = 'openxmllib'
 version = openxmllib.version
-long_description = "%s\n%s\n%s" % (read('README'), read('TODO'), read('HISTORY'))
+long_description = "\n\n".join((read('README'), read('TODO'), read('HISTORY')))
 
 setup(name=name,
       version=version,
-      description='Provides resources to handle OpenXML documents from Python.',
+      description='Provides resources to handle OpenXML documents.',
       long_description=long_description,
       author='Gilles Lenfant',
       author_email='gilles.lenfant@gmail.com',
